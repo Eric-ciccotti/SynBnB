cf580b4e0be86aab77ba00e71df385dd242cbb1c
Eric-ciccotti <eric.ciccotti@gmail.com>
2019-11-17 14:08:36 +0100

    fake data with fixtures and faker library

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,35 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ad;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr-FR');
+
+        for ($i = 1; $i <= 30; $i++) {
+            
+            $title = $faker->sentence(5);
+            $coverImage = $faker->imageUrl(1000, 350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>' . join("</p><p>", $faker->paragraphs(5)) . '</p>'; // <p> dutext </p><p> dutext </p>
+
+            $ad = new Ad();
+            $ad->setTitle($title)
+                ->setSlug("titre-de-l-annonce-n-$i")
+                ->setCoverImage($coverImage)
+                ->setIntroduction($introduction)
+                ->setContent($content)
+                ->setPrice(mt_rand(40, 200))
+                ->setRooms(mt_rand(2, 8));
+
+            $manager->persist($ad);
+        }
 
         $manager->flush();
     }
