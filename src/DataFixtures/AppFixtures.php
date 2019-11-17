bbdd1a5e630393051715b52f00f76dfffebc0cc3
Eric-ciccotti <eric.ciccotti@gmail.com>
2019-11-17 17:26:06 +0100

    association with table ad and image + making of image and controller and twig template

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use App\Entity\Image;
 use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -13,6 +14,7 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr-FR');  //pour avoir des dummy data
 
+        //boucle qui créer les articles
         for ($i = 1; $i <= 30; $i++) {
 
             $title = $faker->sentence(5);
@@ -21,12 +23,22 @@ class AppFixtures extends Fixture
             $content = '<p>' . join("</p><p>", $faker->paragraphs(5)) . '</p>'; // <p> dutext </p><p> dutext </p>
 
             $ad = new Ad();
-            $ad->setTitle($title) 
+            $ad->setTitle($title)
                 ->setCoverImage($coverImage)
                 ->setIntroduction($introduction)
                 ->setContent($content)
                 ->setPrice(mt_rand(40, 200))
                 ->setRooms(mt_rand(2, 8));
+
+            //pour chaque article boucle qui créer des photos
+            for ($j = 1; $j < mt_rand(2, 5); $j++) {
+                $image = new Image();
+                $image->setUrl($faker->imageUrl())
+                    ->setCaption($faker->sentence())
+                    ->setAd($ad);
+
+                $manager->persist($image); // "prepare toi a faire perdurer dans le temps cette image dans la base de donnée"
+            }
 
             $manager->persist($ad);
         }
